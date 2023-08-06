5ea4bc181f8f9618c59b77719d25eb904ae5c86a
Bastian Engel <bastian.engel00@gmail.com>
2023-08-06 18:10:06 +0200

    finish tcp proxy

--- tcp-proxy/src/main.rs
+++ tcp-proxy/src/main.rs
@@ -1,6 +1,25 @@
 use std::env;
-use std::net::{TcpListener};
+use std::thread;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
+fn handle_direction(src: &TcpStream, dst: &TcpStream, on_recv: fn(&[u8], usize) -> &[u8])
+-> thread::JoinHandle<()> {
+    let mut src = src.try_clone().unwrap();
+    let mut dst = dst.try_clone().unwrap();
+
+    thread::spawn(move || {
+        let mut buf = [0u8; 512];
+        loop {
+            let n = src.read(&mut buf).unwrap();
+            if n == 0 {
+                continue;
+            }
+            let res = on_recv(&buf, n);
+            dst.write(&res).unwrap();
+        }
+    })
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,10 +35,34 @@ fn main() {
              src, dst);
 
     let listener = TcpListener::bind(src).unwrap();
+    let (stream_src, sockaddr_src) = listener.accept().unwrap();
+    println!("Accepted incoming connection by peer {:?}", sockaddr_src);
 
-    for stream in listener.incoming() {
-        if let Ok(stream) = stream {
-            println!("Accepted incoming connection by peer {:?}", stream.peer_addr().unwrap());
+    println!("Trying to connect to destination {:?}...", dst);
+    let stream_dst = TcpStream::connect(dst).unwrap();
+
+    let t1 = handle_direction(&stream_src, &stream_dst, |buf, n| {
+        let b = &buf[..n];
+        if let Ok(s) = std::str::from_utf8(b) {
+            println!("{:?}", s);
+        } else {
+            println!("{:02x?}", b);
         }
-    }
+
+        b
+    });
+
+    let t2 = handle_direction(&stream_dst, &stream_src, |buf, n| {
+        let b = &buf[..n];
+        if let Ok(s) = std::str::from_utf8(b) {
+            println!("{:?}", s);
+        } else {
+            println!("{:02x?}", b);
+        }
+
+        b
+    });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
 }
